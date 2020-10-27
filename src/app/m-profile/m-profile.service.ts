1c77c355d624d1cdd1fed7d588c8824c6624b499
Syahir Ghariff <syahirghariff@gmail.com>
2020-10-27 23:17:42 +0800

    development

--- src/app/m-profile/m-profile.service.ts
+++ src/app/m-profile/m-profile.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Profile } from '../model/profile';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -22,7 +24,25 @@ export class MProfileService {
 
 
     findAll() {
-        return this.http.get('/syahirghariff/profile/find_all');
+        return this.http.get('/syahirghariff/profile/find_all').pipe(
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong.
+            console.error(
+                `Backend returned code ${error.status}, ` +
+                `body was: ${error.error}`);
+        }
+        // Return an observable with a user-facing error message.
+        return throwError(
+            'Something bad happened; please try again later.');
     }
 
     deleteById(id) {
